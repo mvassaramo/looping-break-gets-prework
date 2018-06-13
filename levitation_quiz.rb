23f46a2b353f5836127b4a22b741f3c96553cd95
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-06-13 10:36:26 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -3,8 +3,8 @@ def levitation_quiz
 loop do 
   puts "What is the spell thatenacts levitation?"
  answer = gets.chomp
- break if answer == "Wingardium Leviosa"
-end
+ break if answer == "Wingardium Leviosa
+
 
 end
 
